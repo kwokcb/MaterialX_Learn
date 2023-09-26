638d2a1b15db4f41e141a26fe4d8f6672ffd6b50
Bernard Kwok <kwokcb@gmail.com>
2023-09-25 21:01:22 -0400

    1.38.9 update. Start tagging of definitions by version. (#108)

---
+++ pymaterialx/python/docs/html/search/files_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['datatype_2epy_26',['datatype.py',['../datatype_8py.html',1,'']]]
+];
---
+++ pymaterialx/python/docs/html/search/namespaces_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['colorspace_20',['colorspace',['../namespace_material_x_1_1colorspace.html',1,'MaterialX']]],
+  ['datatype_21',['datatype',['../namespace_material_x_1_1datatype.html',1,'MaterialX']]],
+  ['main_22',['main',['../namespace_material_x_1_1main.html',1,'MaterialX']]],
+  ['materialx_23',['MaterialX',['../namespace_material_x.html',1,'']]]
+];
