29bed0c6a28187b0a66a099d6a7282ac0dbdf1cc
Bernard Kwok <kwokcb@gmail.com>
2025-01-22 15:57:20 -0500

    Notebook update.

---
+++ pymaterialx/build_books.sh
@@ -0,0 +1,16 @@
+pip install bpy --upgrade
+pip install usd-core --upgrade
+pip install xmltodict
+for file in *.ipynb; do
+    echo "------------------- Running notebook: $file.. --------------"
+    python -m jupyter execute --allow-errors --inplace "$file" 
+    echo "------------------------------------------------------------"
+done
+#for file in *.ipynb; do
+#    echo "Creating html $file..."
+#    python -m jupyter nbconvert --to html "$file" > html_log.txt
+#done
+#for file in *.ipynb; do
+#    echo "Creating Python $file..."
+#    python -m jupyter nbconvert --to html "$file" > python_log.txt
+#done
