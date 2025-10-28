3169e7f23a1fa8a0204de2c028c22df50d3d7f84
Bernard Kwok <kwokcb@gmail.com>
2025-10-28 12:13:19 -0400

    Add Xml parseing notebook snippet for unescaped characters.

---
+++ pymaterialx/mtlx_xml_parsing.py
@@ -0,0 +1,37 @@
+# %%
+import MaterialX as mx
+import xml.etree.ElementTree as ET
+
+def is_well_formed_xml(xml_string):
+    try:
+        ET.fromstring(xml_string)  
+        print("XML is well-formed.")
+    except ET.ParseError as e:
+        print("XML is not well-formed:", e)    
+        print(xml_string)
+
+print("*"*40)
+test_string = '<?xml version="1.0"?><materialx version="1.39"><implementation name="IM_logical_and_genglsl" nodedef="ND_logical_and" target="genglsl" sourcecode="{{in1}} && {{in2}}"/></materialx>'
+
+doc = mx.createDocument()
+mx.readFromXmlString(doc, test_string)
+is_well_formed_xml(test_string)
+impl = doc.getImplementation("IM_logical_and_genglsl")
+print('- Source code: :', impl.getAttribute("sourcecode"))
+out_string = mx.writeToXmlString(doc)
+print('- Output XML: :', out_string)
+is_well_formed_xml(out_string)
+
+print("*"*40)
+
+test_string = '<?xml version="1.0"?><materialx version="1.39"><implementation name="IM_logical_and_genglsl" nodedef="ND_logical_and" target="genglsl" sourcecode="{{in1}} && < {{in2}}"/></materialx>'
+doc = mx.createDocument()
+mx.readFromXmlString(doc, test_string)
+print('- Source code: :', impl.getAttribute("sourcecode"))
+out_string = mx.writeToXmlString(doc)
+print('- Output Bad XML: :', out_string)
+is_well_formed_xml(out_string)
+
+
+
+
