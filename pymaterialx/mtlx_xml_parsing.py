deb889f2153b8144f8161f6c5280d4035a55a00e
Bernard Kwok <kwokcb@gmail.com>
2025-10-29 11:51:19 -0400

    Add python module inspect and small xml parsing book.

--- pymaterialx/mtlx_xml_parsing.py
+++ pymaterialx/mtlx_xml_parsing.py
@@ -5,10 +5,11 @@ import xml.etree.ElementTree as ET
 def is_well_formed_xml(xml_string):
     try:
         ET.fromstring(xml_string)  
-        print("XML is well-formed.")
+        print("- XML : is well-formed.")
     except ET.ParseError as e:
-        print("XML is not well-formed:", e)    
-        print(xml_string)
+        print("- XML :", e)    
+    print(xml_string)
+    print('\n')
 
 print("*"*40)
 test_string = '<?xml version="1.0"?><materialx version="1.39"><implementation name="IM_logical_and_genglsl" nodedef="ND_logical_and" target="genglsl" sourcecode="{{in1}} && {{in2}}"/></materialx>'
@@ -19,7 +20,6 @@ is_well_formed_xml(test_string)
 impl = doc.getImplementation("IM_logical_and_genglsl")
 print('- Source code: :', impl.getAttribute("sourcecode"))
 out_string = mx.writeToXmlString(doc)
-print('- Output XML: :', out_string)
 is_well_formed_xml(out_string)
 
 print("*"*40)
@@ -29,7 +29,6 @@ doc = mx.createDocument()
 mx.readFromXmlString(doc, test_string)
 print('- Source code: :', impl.getAttribute("sourcecode"))
 out_string = mx.writeToXmlString(doc)
-print('- Output Bad XML: :', out_string)
 is_well_formed_xml(out_string)
 
 
